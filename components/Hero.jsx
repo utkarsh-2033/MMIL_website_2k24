f4011fcb05221ea97d66f490105527629decedb5
Parth Chaturvedi <parthchaturvedi0204@gmail.com>
2024-11-19 00:54:21 +0530

    starter files uploaded

---
+++ components/Hero.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import gsap from 'gsap';
+import { useGSAP } from '@gsap/react';
+import { Header } from './Header';
+
+export const Hero = () => {
+  return (
+    <>
+    <Header />
+    </>
+  )
+}
---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import { Hero } from '../components/Hero'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Hero/>
+    </>
+  )
+}
+
+export default App
